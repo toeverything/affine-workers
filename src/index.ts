63bce45f5a68a7150d0779b18c0203ad4ff25339
Flrande <flrande.ch@gmail.com>
2023-12-04 11:00:58 +0800

    feat: support link preview

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,140 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+interface RequestData {
+	url: string;
 }
 
+interface ResponseData {
+	url: string;
+	title?: string;
+	siteName?: string;
+	description?: string;
+	images?: string[];
+	mediaType?: string;
+	contentType?: string;
+	charset?: string;
+	videos?: string[];
+	favicons?: string[];
+}
+
+const ALLOWED_ORIGINS = [
+	'http://localhost:5173',
+	'https://try-blocksuite.vercel.app/',
+	'https://blocksuite-toeverything.vercel.app/',
+	'https://affine.pro',
+	'https://app.affine.pro',
+	'https://insider.affine.pro',
+	'https://affine.fail',
+];
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request): Promise<Response> {
+		const origin = request.headers.get('Origin');
+		console.log(origin);
+
+		if (request.method === 'OPTIONS') {
+			return handleOptionsRequest(request);
+		}
+
+		if (request.method !== 'POST') {
+			return new Response(null, {
+				status: 405,
+				statusText: 'Method Not Allowed',
+			});
+		}
+
+		const requestBody: RequestData = await request.json();
+		let targetURL: string = requestBody.url;
+
+		if (!/^https?:\/\//.test(targetURL)) {
+			targetURL = 'http://' + targetURL;
+		}
+
+		const response: Response = await fetch(targetURL, {
+			cf: {
+				cacheTtl: 43200,
+				cacheEverything: true,
+			},
+		});
+
+		const res: ResponseData = {
+			url: response.url,
+			images: [],
+			videos: [],
+			favicons: [],
+		};
+
+		if (response.body) {
+			const rewriter = new HTMLRewriter()
+				.on('meta', {
+					element(element) {
+						const property = element.getAttribute('property');
+						const content = element.getAttribute('content');
+						if (property && content) {
+							switch (property.toLowerCase()) {
+								case 'og:title':
+									res.title = content;
+									break;
+								case 'og:site_name':
+									res.siteName = content;
+									break;
+								case 'og:description':
+									res.description = content;
+									break;
+								case 'og:image':
+									res.images?.push(content);
+									break;
+								case 'og:video':
+									res.videos?.push(content);
+									break;
+								case 'og:type':
+									res.mediaType = content;
+									break;
+								// 添加更多的 OpenGraph 标签处理逻辑
+							}
+						}
+					},
+				})
+				.on('link', {
+					element(element) {
+						if (element.getAttribute('rel') === 'icon') {
+							const href = element.getAttribute('href');
+							if (href) {
+								res.favicons?.push(href);
+							}
+						}
+					},
+				});
+
+			await rewriter.transform(response).text();
+		}
+
+		const json = JSON.stringify(res);
+		return new Response(json, {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+				...getCorsHeaders(origin),
+			},
+		});
 	},
 };
+
+function handleOptionsRequest(request: Request): Response {
+	const origin = request.headers.get('Origin');
+	return new Response(null, {
+		headers: {
+			...getCorsHeaders(origin),
+			'Access-Control-Allow-Methods': 'POST, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type',
+		},
+	});
+}
+
+function getCorsHeaders(origin: string | null): { [key: string]: string } {
+	if (origin && ALLOWED_ORIGINS.includes(origin)) {
+		return {
+			'Access-Control-Allow-Origin': origin,
+			'Access-Control-Allow-Credentials': 'true',
+		};
+	} else {
+		return {};
+	}
+}
